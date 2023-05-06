5bb16cea3c3dbf5f6d03bcab3c7cb20a83164293
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-06 02:42:00 -0300

    atividade 4

--- AluraTravel/AluraTravel/Views/CelluleTripsView.swift
+++ AluraTravel/AluraTravel/Views/CelluleTripsView.swift
@@ -10,21 +10,30 @@ import Inject
 
 struct CelluleTripsView: View {
     @ObservedObject private var iO = Inject.observer
+    
+    @Environment(\.horizontalSizeClass) var _horizontalSizeClass
+    
     var body: some View {
+        let fontSize: CGFloat = self._horizontalSizeClass == .compact ? 22 : 27
+        let imgHeight: CGFloat = self._horizontalSizeClass == .compact ? 250 : 500
+        
         List(trips) { trip in
             VStack(alignment: .leading) {
                 Text(trip.title)
+                    .font(.custom("Avenir Light", size: fontSize))
                 Image(trip.image)
                     .resizable()
-                    .frame(height: 250)
+                    .frame(height: imgHeight)
                     .cornerRadius(10)
                 
                 HStack {
                     Text(trip.numberOfDays)
+                        .font(.custom("Avenir Light", size: fontSize))
                     
                     Spacer()
                     
                     Text(trip.value)
+                        .font(.custom("Avenir Light", size: fontSize))
                 }
             }
             .listRowSeparator(.hidden)
