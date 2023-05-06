66bb5a241ce6bb143c1c4a5a4d88121555626a88
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-05 22:51:02 -0300

    atividade 3

--- AluraTravel/AluraTravel/ContentView.swift
+++ AluraTravel/AluraTravel/ContentView.swift
@@ -4,31 +4,39 @@ import Inject
 struct ContentView: View {
     @ObservedObject private var iO = Inject.observer
     var body: some View {
-        VStack {
-            Text("AluraTrips")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 20))
-            Text("Especial")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Book", size: 20))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            Text("Brasil")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 23))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            List {
-                Text("São Paulo")
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Atibaia")
+
+        GeometryReader { view in
+            VStack {
+                VStack {
+                    Text("AluraTrips")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                    Text("Especial")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("Brasil")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                   
+                }
+                .frame(width: view.size.width, height: 100, alignment: .top)
+                .background(Color.purple)
+           
+                
+                List {
+                    Text("São Paulo")
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibai")
+                }
             }
         }
-        .background(Color.purple)
-        
-        
         .enableInjection()
     }
+        
         
 }
