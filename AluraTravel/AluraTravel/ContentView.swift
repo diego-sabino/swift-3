298a1afe6ebbce214e299ee4a30b02dbaa1f9a48
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-05 22:35:50 -0300

    atividade 2

--- AluraTravel/AluraTravel/ContentView.swift
+++ AluraTravel/AluraTravel/ContentView.swift
@@ -6,19 +6,29 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("AluraTrips")
+                .foregroundColor(Color.white)
+                .font(.custom("Avenir Black", size: 20))
             Text("Especial")
+                .foregroundColor(Color.white)
+                .font(.custom("Avenir Book", size: 20))
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 30)
             Text("Brasil")
+                .foregroundColor(Color.white)
+                .font(.custom("Avenir Black", size: 23))
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 30)
+            List {
+                Text("São Paulo")
+                Text("Rio de Janeiro")
+                Text("Ceará")
+                Text("Atibaia")
+            }
         }
-        
-        List {
-            Text("São Paulo")
-            Text("Rio de Janeiro")
-            Text("Ceará")
-            Text("Atibaia")
-        }
-        
+        .background(Color.purple)
         
         
         .enableInjection()
     }
+        
 }
